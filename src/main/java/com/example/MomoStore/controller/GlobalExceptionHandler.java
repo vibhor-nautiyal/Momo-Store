56879d842700217b412cafbb3ff422ddbffec7b2
Vibhor Nautiyal <vinautiyal@deloitte.com>
2021-06-15 23:43:53 +0530

    exceptions handled

---
+++ src/main/java/com/example/MomoStore/controller/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.example.MomoStore.controller;
+
+
+import com.example.MomoStore.exception.*;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DishNotFoundException.class)
+    public ResponseEntity<String> dishNotFound(DishNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> userNotFound(UserNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<String> orderNotFound(OrderNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(QuantityException.class)
+    public ResponseEntity<String> quantityException(QuantityException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(WrongTimeException.class)
+    public ResponseEntity<String> wrongTime(WrongTimeException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> duplicateEntry(ConstraintViolationException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
